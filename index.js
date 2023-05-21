e08423ae123e02d37e0811ca577948007898636b
Hanson <hanson.hua903@gmail.com>
2023-05-20 22:53:06 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers,name){
+    return drivers.filter(drivers => drivers.toLowerCase()=== name.toLowerCase())
+};
+function fuzzyMatch(drivers,letters){
+    return drivers.filter(drivers => drivers.startsWith(letters))
+};
+function matchName(drivers,name){
+    return drivers.filter(drivers => drivers.name===name)
+};
